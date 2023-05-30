eabf08f847870b68bb3166bdedaba86aefb7d6ff
KomalKReddy <82363361+komalreddy3@users.noreply.github.com>
2023-05-30 15:17:09 +0530

    Update Content Based Recommendation System.py

--- pages/Content Based Recommendation System.py
+++ pages/Content Based Recommendation System.py
@@ -32,7 +32,7 @@ add_bg_from_local('backgroundj3.jpg')
 
 
 sig = load(open('sig.pkl','rb'))
-df_act = load(open('df.pkl','rb'))
+df_act = load(open('MRS1/df.pkl','rb'))
 
     # Reverse mapping of indices and movie titles
 indices = pd.Series(df_act.index, index = df_act['title']).drop_duplicates()
